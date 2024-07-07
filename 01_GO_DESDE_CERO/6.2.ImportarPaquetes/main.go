f28a6f4320d2ac854f3213b7df6015af4f4d0c03
118.hidalgo.leonel.1358 <118.hidalgo.leonel.1358@gmail.com>
2024-07-07 20:08:04 +0000

    Se aprende cómo importar nuestros propios modulos y de terceros

---
+++ 01_GO_DESDE_CERO/6.2.ImportarPaquetes/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	//* Tenemos que importar con la url acá y aclarar qué carpeta interna quiero usar
+	"strings"
+
+	"github.com/Lahkpom/mainModules/slices"
+	"rsc.io/quote"
+)
+
+//! PARA PODER USAR LOS MÓDULOS O PAQUETES QUE NOSOTROS MISMO CREAMOS, HAY QUE SUBIRLOS A UN REPOSITORIO DE GITHUB E IMPORTARLO
+//! DESDE ALLÍ, PARA ESO TENEMOS QUE EJECUTAR
+// go mod init github.com/usuario/proyecto
+//* Se crea el archivo mod de donde go busca las cosas
+
+func main() {
+	list := []string{"perro", "Gato", "gorila", "mono", "guacamayo", quote.Hello()}
+	slices.Includes(list, "Gato")
+	//fmt.Println(aux)
+	//* Esto va a filtrar aquellos valores que empiecen con una letra determinada y retorna un bool de si encontró o no el valor
+	slices.Filter(list, func(v string) bool {
+		return strings.HasPrefix(strings.ToLower(v), "h")
+	})
+}
