266a83b6733335b3ddf536a77173f24c6bd87f44
118.hidalgo.leonel.1358 <118.hidalgo.leonel.1358@gmail.com>
2024-07-14 12:08:47 +0000

    Pruebas con paquetes

--- 02_POO_GO/2.2.ConstructorNew/go.mod
+++ 02_POO_GO/2.2.ConstructorNew/go.mod
@@ -2,4 +2,4 @@ module github.com/Lahkpom/myModules
 
 go 1.22.5
 
-require github.com/Lahkpom/myModules/courses v0.0.0-20240714002904-9774e6bc618c
+require github.com/Lahkpom/myModules/courses v0.0.0-20240714114617-d07b5fb040d7
--- 02_POO_GO/2.2.ConstructorNew/main.go
+++ 02_POO_GO/2.2.ConstructorNew/main.go
@@ -26,22 +26,29 @@ func main() {
 
 	c1 := courses.NewCourse("Prueba", 50, false)
 
-	c1.Classes = []courses.Class{
-		{Name: "Clase 1", Description: "Descripción de la clase 1"},
-		{Name: "Clase 2", Description: "Descripción de la clase 2"},
-		{Name: "Clase 3", Description: "Descripción de la clase 3"},
-	}
+	c1.AddClass("Clase 1", "Descripción de la clase 1")
+	c1.AddClass("Clase 2", "Descripción de la clase 2")
+	c1.AddClass("Clase 3", "Descripción de la clase 3")
 
-	c24 := courses.Class{Name: "Clase 4", Description: "Descripción de la clase 4"}
-	c25 := courses.Class{Name: "Clase 5", Description: "Descripción de la clase 5"}
+	/*
+		c1.Classes = []courses.Class{
+			{Name: "Clase 1", Description: "Descripción de la clase 1"},
+			{Name: "Clase 2", Description: "Descripción de la clase 2"},
+			{Name: "Clase 3", Description: "Descripción de la clase 3"},
+		}
+	*/
 
-	c1.Classes = append(c1.Classes, c24, c25)
+	// c24 := courses.Class{Name: "Clase 4", Description: "Descripción de la clase 4"}
+	// c25 := courses.Class{Name: "Clase 5", Description: "Descripción de la clase 5"}
+
+	c1.AddClass("Clase 4", "Descripción de la clase 4")
+	c1.AddClass("Clase 5", "Descripción de la clase 5")
 
 	c1.PrintClasses()
-	fmt.Printf("El precio del curso es %v\n", c1.Price)
+	fmt.Printf("El precio del curso es %v\n", c1.GetPrice())
 
 	c1.ChangePrice(100)
-	fmt.Printf("El precio del curso actualizado es: %f\n", c1.Price)
+	fmt.Printf("El precio del curso actualizado es: %f\n", c1.GetPrice())
 
 	//c2 := courses.NewCourse("Prueba 2", 50)
 	//fmt.Println(c2)
